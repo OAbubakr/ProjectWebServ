8be0152b824d39f46569b48b614889fd228c744e
mrbioeng <mrbioeng@gmail.com>
2017-05-31 13:54:07 +0200

    mohamed abdelbaset get instructors by branch id

--- src/main/java/bean/Instructor.java
+++ src/main/java/bean/Instructor.java
@@ -16,7 +16,16 @@ public class Instructor {
     private int instuctorId;
     private String instructorName;
     private int branchId;
+    private String imagePath;
 
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+    
     public int getInstuctorId() {
         return instuctorId;
     }
