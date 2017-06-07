49d6dfccd340e91b19a84e08e57121ba117b1ceb
mrbioeng <mrbioeng@gmail.com>
2017-06-07 04:50:44 +0200

    Merge origin/master
    Conflicts:
    	pom.xml
    	src/main/java/com/mycompany/restfulspring/InstructorsByBranch.java
    	src/main/java/second/DaoInstance.java

--- src/main/java/com/mycompany/restfulspring/TokenGeneratorController.java
+++ src/main/java/com/mycompany/restfulspring/TokenGeneratorController.java
@@ -5,26 +5,24 @@
  */
 package com.mycompany.restfulspring;
 
-import com.nimbusds.jose.EncryptionMethod;
 import com.nimbusds.jose.JOSEException;
-import com.nimbusds.jose.JWEAlgorithm;
-import com.nimbusds.jose.JWEHeader;
-import com.nimbusds.jose.JWEObject;
 import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.JWSHeader;
 import com.nimbusds.jose.JWSSigner;
 import com.nimbusds.jose.KeyLengthException;
-import com.nimbusds.jose.Payload;
-import com.nimbusds.jose.crypto.DirectEncrypter;
 import com.nimbusds.jose.crypto.MACSigner;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
 import dto.LoginRequest;
 import dto.Response;
+import java.text.ParseException;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.crypto.SecretKey;
+import javax.servlet.ServletContext;
 import net.minidev.json.JSONObject;
-import org.springframework.http.MediaType;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -32,7 +30,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import second.DaoInstance;
 import second.LoginDAO;
-import security_aspect.SecurityKeyInstance;
 
 /**
  *
@@ -41,55 +38,51 @@ import security_aspect.SecurityKeyInstance;
 @RestController
 public class TokenGeneratorController {
 
+    private ServletContext servletContext;
+    
     @RequestMapping(value = "/getToken",
             method = RequestMethod.POST,
-//            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
             headers = "Accept=application/json")
-
     public Response getToken(@RequestBody LoginRequest request) {
 
         Response response = null;
         int id;
-
-        System.out.println("request " + request.getUserName() + " "+ request.getUserType() + " "+ request.getPassword());
         
+        System.out.println("request " + request.getUserName() + " " + request.getUserType() + " " + request.getPassword());
+
         //login logic
         LoginDAO loginDao = DaoInstance.getInstance().getLoginDao();
         response = loginDao.getUserId(request.getUserType(), request.getUserName(), request.getPassword());
-        if (response.getStatus().equals("success")) {
+
+        if (response.getStatus().equals(Response.sucess)) {
             id = (int) response.getResponseData();
             System.out.println("id " + id);
             //create access token
             try {
-
+String accessKey = (String)servletContext.getAttribute("accessKey");
+String refreshKey = (String)servletContext.getAttribute("refreshKey");
                 // Generate 256-bit AES key for HMAC as well as encryption
-                SecretKey secretKey = SecurityKeyInstance.getEncryptionKey();
-                // Create HMAC signer
-                JWSSigner signer = new MACSigner(secretKey.getEncoded());
+                long accessExpiryDateInMillis = System.currentTimeMillis() + (60000); //valid for one week
+                long refreshExpiryDateInMillis = System.currentTimeMillis() + (60000); //valid for one week
 
-                // Prepare JWT with claims set 7 * 24 * 60 * 60 * 1000
-                long expiryDateInMillis = System.currentTimeMillis() + (60000); //valid for one week
+                String accessToken = getAccessToken(security.SecurityManager.getAccessKey(),
+                        accessExpiryDateInMillis,
+                        String.valueOf(id),
+                        String.valueOf(request.getUserType()));
+                String refreshToken = getRefreshToken(security.SecurityManager.getRefreshKey(),
+                        refreshExpiryDateInMillis,
+                        String.valueOf(id),
+                        String.valueOf(request.getUserType()));
 
-                JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
-                        .expirationTime(new Date(expiryDateInMillis))
-                        .claim("id", String.valueOf(id))
-                        .claim("type", String.valueOf(request.getUserType()))
-                        .build();
-
-                SignedJWT signedJWT = new SignedJWT(
-                        new JWSHeader(JWSAlgorithm.HS256), claimsSet);
-
-                // Apply the HMAC
-                signedJWT.sign(signer);
-                String accessToken = signedJWT.serialize();
-
+                //create the refresh token
                 response = new Response();
-                response.setStatus("success");
+                response.setStatus(Response.sucess);
                 response.setError(null);
                 JSONObject responseData = new JSONObject();
                 responseData.put("access_token", accessToken);
+                responseData.put("refresh_token", refreshToken);
                 responseData.put("token_type", "bearer");
-                responseData.put("expiry_date", expiryDateInMillis);
+                responseData.put("expiry_date", accessExpiryDateInMillis);
                 response.setResponseData(responseData);
 
             } catch (KeyLengthException ex) {
@@ -103,6 +96,105 @@ public class TokenGeneratorController {
         return response;
     }
 
+    @RequestMapping(value = "/renewAccessToken",
+            method = RequestMethod.POST,
+            headers = "Accept=application/json")
+    public Response renewAccessToken(@RequestParam(value = "refreshToken", required = true) String refreshToken) {
+
+        Response response = new Response();
+        response.setError(Response.INVALID_REFRESH_TOKEN);
+        response.setStatus(Response.failure);
+        response.setResponseData(null);
+
+        try {
+            //validate refresh token
+            SecretKey secretKey = security.SecurityManager.getRefreshKey();
+            JSONObject refreshTokenPayload = security.SecurityManager.validateToken(refreshToken, secretKey);
+
+            String id = (String) refreshTokenPayload.get("id");
+            String type = (String) refreshTokenPayload.get("type");
+            long accessExpiryDateInMillis = System.currentTimeMillis() + (60000); //valid for one week
+            String accessToken = getAccessToken(security.SecurityManager.getAccessKey(),
+                    accessExpiryDateInMillis, id, type);
+
+            response.setStatus(Response.sucess);
+            response.setError(null);
+            JSONObject responseData = new JSONObject();
+            responseData.put("access_token", accessToken);
+            responseData.put("token_type", "bearer");
+            responseData.put("expiry_date", accessExpiryDateInMillis);
+            response.setResponseData(responseData);
+
+        } catch (KeyLengthException ex) {
+            ex.printStackTrace();
+
+        } catch (JOSEException ex) {
+            ex.getMessage();
+            if (ex.getMessage().equals("expiredToken")) {
+                System.out.println("jose time exception");
+                response.setError(Response.EXPIRED_REFRESH_TOKEN);
+            }
+
+            ex.printStackTrace();
+        } catch (ParseException ex) {
+            ex.printStackTrace();
+
+        }
+
+        return response;
+
+    }
+
+    @RequestMapping(value = "/renewRefreshToken",
+            method = RequestMethod.POST,
+            headers = "Accept=application/json")
+    public Response renewRefreshToken(@RequestParam(value = "refreshToken", required = true) String refreshToken) {
+        
+        return null;
+
+    }
+
+    private String getAccessToken(SecretKey secretKey, long expiryDateInMillis, String id, String type)
+            throws KeyLengthException, JOSEException {
+
+        JWSSigner signer = new MACSigner(secretKey.getEncoded());
+
+        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+                .expirationTime(new Date(expiryDateInMillis))
+                .claim("id", id)
+                .claim("type", type)
+                .build();
+
+        SignedJWT signedJWT = new SignedJWT(
+                new JWSHeader(JWSAlgorithm.HS256), claimsSet);
+
+        // Apply the HMAC
+        signedJWT.sign(signer);
+        return signedJWT.serialize();
+
+    }
+
+    private String getRefreshToken(SecretKey refreshKey, long expiryDateInMillis, String id, String type)
+            throws KeyLengthException, JOSEException {
+
+        JWSSigner signer = new MACSigner(refreshKey.getEncoded());
+
+        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+                .expirationTime(new Date(expiryDateInMillis))
+                .claim("id", id)
+                .claim("type", type)
+                .build();
+
+        SignedJWT signedJWT = new SignedJWT(
+                new JWSHeader(JWSAlgorithm.HS256), claimsSet);
+
+        // Apply the HMAC
+        signedJWT.sign(signer);
+        return signedJWT.serialize();
+
+    }
+
+    /*
     @RequestMapping(value = "/getTokenEncrypted", method = RequestMethod.GET, headers = "Accept=application/json")
     public String getTokenEncrypted(
             @RequestParam(value = "userName", required = true) String userName,
@@ -155,8 +247,8 @@ public class TokenGeneratorController {
         }
         return s;
     }
-
-    /*
+     */
+ /*
     @RequestMapping(value = "/getToken", 
             method = RequestMethod.POST, 
             consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
